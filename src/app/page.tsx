574ec171226226d51a18ebf70528f813044a4225
dovushman <52638904+dovushman@users.noreply.github.com>
2025-04-01 22:50:38 -0400

    vercel sticky fix 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function HomePage() {
-  useEffect(() => {
-    const setVh = () => {
-      document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
-    };
-    setVh();
-    window.addEventListener("resize", setVh);
-    return () => window.removeEventListener("resize", setVh);
-  }, []);
+  // useEffect(() => {
+  //   const setVh = () => {
+  //     document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+  //   };
+  //   setVh();
+  //   window.addEventListener("resize", setVh);
+  //   return () => window.removeEventListener("resize", setVh);
+  // }, []);
 
   return (
     <main>
