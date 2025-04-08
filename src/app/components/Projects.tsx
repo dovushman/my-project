2f82968e8ca6eb050dbcffb4e0e62cdd4991ed49
dovushman <52638904+dovushman@users.noreply.github.com>
2025-04-08 15:30:08 -0400

    seperate projectList file

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useTheme } from "../ThemeContext";
 import { useState, useEffect } from "react";
+import { projectList as projects } from "../projectList"; // Import the project list
 
 export default function Projects() {
   const { theme } = useTheme();
@@ -26,89 +27,7 @@ export default function Projects() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  const projects = [
-    {
-      id: 0,
-      title: "Pokédex App",
-      description: "A React Native app for searching Pokémon.",
-      fullDescription:
-        "A full-featured Pokédex with searching, filters and information regarding pokemon. Includes a comprehensive 'Whos that Pokemon?' with AI image recognition and textual description. Also includes a team builder feature where users can manually enter a team or use a custom AI model to help create a competitively viable team.",
-      link: "#",
-      image: "/images/tempPokedexPhoto.png",
-      techStack: ["reactNative", "expo", "sqlite"],
-    },
-    {
-      id: 1,
-      title: "7Factor AWS Analysis",
-      description: "Web Application to analyze and help lower AWS costs.",
-      fullDescription:
-        "Web applications with a Python backend and React frontend, providing a solution to reduce AWS costs.",
-      link: "#",
-      image: "/images/7FactorSmallLogo.png",
-      techStack: ["react", "python", "express"],
-    },
-    {
-      id: 2,
-      title: "Sharpify: Image-Enhancing Website",
-      description: "A web app for enhancing images quality.",
-      fullDescription:
-        "Web app to enhance image quality and be a safe storing place for images.",
-      link: "#",
-      image: "/images/SharpifyCoverImage.png",
-      techStack: ["react", "nodejs", "mongodb", "firebase"],
-    },
-    {
-      id: 3,
-      title: "Brigham and Women's Hospital Desktop Application",
-      description: "A desktop application for hospital management.",
-      fullDescription:
-        "A desktop application for hospital management, including staff communication, management requests, and adjustable signage.",
-      link: "#",
-      image: "/images/portfolio.jpg",
-      techStack: ["java", "javafx", "postgresql", "figma"],
-    },
-    {
-      id: 4,
-      title: "MOSE Mobile App Prototype",
-      description: "A prototype and design for MOSE floodgates in Venice, Italy.",
-      fullDescription:
-        "An interactive mock-up for the Commissario Straordinario MOSE to help alert the public of floodgate closures.",
-      link: "#",
-      image: "/images/team-builder.jpg",
-      techStack: ["xd"],
-    },
-    {
-      id: 5,
-      title: "HomeHub: Smart Home Management",
-      description: "A web application to streamline roommate management ",
-      fullDescription:
-        "A web application to streamline roommate management, including a calendar, task manager, and shopping list.",
-      link: "#",
-      image: "/images/team-builder.jpg",
-      techStack: ["react", "nodejs", "mongodb", "bootstrap"],
-    },
-    {
-      id: 6,
-      title: "LegUp: Note Sharing Website",
-      description: "Note sharing website for textbook material and collaborative learning platform",
-      fullDescription:
-        "Developed, with a team, a web and mobile application: note sharing for textbook material and collaborative learning platform. Implemented the Google Books API and constructed algorithms that identified requested book and contained submitted notes. Presented the above application at Google Cambridge to Google employees and interns.",
-      link: "#",
-      image: "/images/team-builder.jpg",
-      techStack: ["javascript", "html5", "css3", "bootstrap", "heroku"],
-    },
-    {
-      id: 7,
-      title: "myCoverage",
-      description: "Mobile fitness application",
-      fullDescription:
-        "Android mobile fitness app to record daily steps taken, time duration of runs, and display running history.",
-      link: "#",
-      image: "/images/team-builder.jpg",
-      techStack: ["kotlin", "android"],
-    },
-  ];
-
+  
   const customIcons = {
     expo: "/icons/expo-svgrepo-com.svg",
     reactNative: "/icons/react-native-1.svg",
