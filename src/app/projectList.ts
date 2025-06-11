19adf436af4c37cd2b2162b9d9137fbf9ad556ac
dovushman <52638904+dovushman@users.noreply.github.com>
2025-06-10 21:13:10 -0400

    updated url to remove page

--- src/app/projectList.ts
+++ src/app/projectList.ts
@@ -5,7 +5,7 @@ export const projectList = [
     description: "Mobile Pokédex app with AI searching and team builder.",
     fullDescription:
       "A React-Native mobile Pokédex app with searching, filters and information regarding Pokémon. The app includes a comprehensive “Who's that Pokémon?” feature with AI image recognition and textual description. It also includes a team builder feature where users can manually enter a team or use a custom AI model to help create a competitively viable team.",
-    link: "/projects/pokedexPage",
+    link: "/projects/pokedex",
     image: "/static/images/Pokedex/tempPokedexPhoto.png",
     techStack: ["reactNative", "expo", "sqlite"],
   },
@@ -15,7 +15,7 @@ export const projectList = [
       description: "Web Application to analyze and help lower AWS costs.",
       fullDescription:
         "A web application with a Python backend and React frontend to help reduce AWS costs. Specifically, the application examines different AWS EC2 instances to identify opportunities for downsizing storage configurations and minimizing expenses. This project was completed as my Major Qualifying Project (MQP) at WPI, sponsored by 7Factor.",
-      link: "/projects/7FactorPage",
+      link: "/projects/7Factor",
       image: "/static/images/7Factor/7FactorSmallLogo.png",
       techStack: ["react", "python", "axios"],
     },
@@ -25,7 +25,7 @@ export const projectList = [
       description: "A full-stack web app for enhancing images quality and managing images.",
       fullDescription:
         "A full-stack web application designed to enhance image quality, and securely store and manage images. Built with a React frontend and Node.js backend, the app integrates MongoDB to store user data and Firebase for image storage. The image enhancements are powered by a custom configuration of the Deep Image AI API.",
-      link: "/projects/sharpifyPage",
+      link: "/projects/sharpify",
       image: "/static/images/Sharpify/SharpifyCoverImage.png",
       techStack: ["react", "nodejs", "mongodb", "firebase", "express"],
     },
@@ -35,7 +35,7 @@ export const projectList = [
       description: "A desktop application for hospital management created for Brigham and Women's Hospital.",
       fullDescription:
         "A prototype desktop application to assist hospital staff with communication, request handling, and dynamic signage management. Built using Java, JavaFX, and PostgreSQL, the application was developed for Brigham and Women’s Hospital as part of a collaborative effort to enhance hospital operations. ",
-      link: "/projects/hospitalPage",
+      link: "/projects/brigham&womens",
       image: "/static/images/B&W/B&WLogo.png",
       techStack: ["java", "postgresql", "figma"],
     },
@@ -45,7 +45,7 @@ export const projectList = [
       description: "An interactive mobile mock-up for the MOSE floodgates in Venice, Italy.",
       fullDescription:
         "An interactive mock-up designed in collaboration with the Commissario Straordinario MOSE in Venice, Italy. The app aims to improve communication around MOSE floodgate operations and high-tide events for residents and visitors. Built using Adobe XD, the mock-up simulates a range of real-world scenarios to ensure usability and effectiveness. This project was completed as my Interactive Qualifying Project (IQP) at WPI.",
-      link: "/projects/mosePage",
+      link: "/projects/mose",
       image: "/static/images/MOSECoverImage.png",
       techStack: ["xd"],
     },
@@ -55,7 +55,7 @@ export const projectList = [
       description: "A web application for managing shared living spaces.",
       fullDescription:
         "A web application built to simplify the logistics of shared living. HomeHub combines a joint calendar, task manager, and shopping list into a single platform to help roommates stay organized and communicate effectively. It is being built with React, React-Bootstrap, Node.js and MongoDB.",
-      link: "/projects/homehubPage",
+      link: "/projects/homehub",
       image:"/static/images/HomeHubCoverImage.png",
       techStack: ["react", "nodejs", "mongodb", "bootstrap"],
     },
@@ -65,7 +65,7 @@ export const projectList = [
       description: "Note sharing website for textbook material and collaborative learning platform",
       fullDescription:
         "LegUp is a web-based note-sharing and collaborative learning platform built to help students exchange textbook-based notes and study materials more effectively. It integrates the Google Books API to match notes with specific books based on title and author and ISBN number. The project was developed using JavaScript, HTML5, CSS3, and Bootstrap. The project was presented at Google Cambridge to employees and interns.",
-      link: "/projects/legupPage",
+      link: "/projects/legup",
       image: "/static/images/LegUpCoverImage.png",
       techStack: ["javascript", "html5", "css3", "bootstrap", "heroku"],
     },
@@ -75,7 +75,7 @@ export const projectList = [
       description: "Google Chrome Extension to enable automatic closing of paired symbols",
       fullDescription:
         "A Google Chrome Extension to enable automatic closing of paired symbols (e.g., brackets, parentheses) in text areas and code editors. The extension uses a content script to monitor user input and automatically insert the closing symbol when the opening symbol is typed.",
-      link: "/projects/enclosePage",
+      link: "/projects/enclose",
       image: "/static/images/encloseCoverImage.png",
       techStack: ["javascript", "html5", "css3"],
     },
@@ -85,7 +85,7 @@ export const projectList = [
       description: "Mobile fitness application",
       fullDescription:
         "Android mobile fitness app to record daily steps taken, time duration of runs, and display running history. This application was my first time making a mobile application and was created using Kotlin.",
-      link: "/projects/mycoveragePage",
+      link: "/projects/mycoverage",
       image: "/static/images/MyCoverageCoverPhoto.jpg",
       techStack: ["kotlin", "android"],
     },
